cff6c5e69e37b380047a42bc4b25fc14e2cf43d4
Denilza <denylzalopes@gmail.com>
2026-03-15 03:19:08 +0100

    fix: exclude JPA via @SpringBootApplication annotation

--- backend-microservices/notification-service/src/main/java/com/accessmap/notificationservice/NotificationServiceApplication.java
+++ backend-microservices/notification-service/src/main/java/com/accessmap/notificationservice/NotificationServiceApplication.java
@@ -2,8 +2,15 @@ package com.accessmap.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class,
+    JpaRepositoriesAutoConfiguration.class
+})
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
